f8305f6f285969275c8f48b9f5f7c826b10beb6c
Tobias Ottenweller <tobi@localhost.(none)>
2011-04-08 20:11:01 +0200

    -f CLI option added

--- graphserver_tools/process.py
+++ graphserver_tools/process.py
@@ -14,21 +14,24 @@ from graphserver_tools import write_results
 from graphserver_tools import process_routes
 
 
-def build_base_data(db_conn_string):
-    gtfs_filename = os.path.join('01-Basisdaten', 'transit_data.zip')
-    osm_xml_filename = os.path.join('01-Basisdaten', 'streets.osm')
+def build_base_data(dir_name, db_conn_string):
+    gtfs_filename = os.path.join(dir_name, 'transit_data.zip')
+    osm_xml_filename = os.path.join(dir_name, 'streets.osm')
 
     if not os.path.exists(osm_xml_filename):
         print('ERROR: no osm data found!')
         exit(-1)
 
     if not os.path.exists(gtfs_filename):
-        print('ERROR: no transit data found!...')
+        print('ERROR: no transit data found!')
         exit(-1)
 
     print('importing data into (graph) databases...')
     import_base_data.create_gs_datbases(osm_xml_filename, gtfs_filename, db_conn_string)
+
     import_base_data.add_missing_stops(db_conn_string)
+
+    print('deleting orphan nodes...')
     import_base_data.delete_orphan_nodes(db_conn_string)
 
     print('linking transit to osm data...')
@@ -38,8 +41,10 @@ def build_base_data(db_conn_string):
 def main():
     from optparse import OptionParser
 
-    usage = """usage: python gst_process calculation-folder.\nNote: a special file hirarchie is neccessary in this folder. See documentaion for further information."""
+    usage = """usage: python gst_process calculation-folder.
+               Note: a special file hirarchie is neccessary in this folder. See documentaion for further information."""
     parser = OptionParser(usage=usage)
+    parser.add_option("-f", "--forceinit", action="store_true", help="forces to reload all data", dest="overwrite", default=False)
 
     (options, args) = parser.parse_args()
 
@@ -48,6 +53,7 @@ def main():
         exit(-1)
 
     dir_name = args[0]
+    overwrite = options.overwrite
 
     times_filename = os.path.join(dir_name, 'times.csv')
     points_filename = os.path.join(dir_name, 'points.csv')
@@ -60,8 +66,7 @@ def main():
         parser.print_help()
         exit(-1)
 
-
-     # read the configuration
+    # read the configuration
     defaults = { 'time-step':'240',
                  'max-walk':'11080',
                  'walking-reluctance':'20',
@@ -75,15 +80,29 @@ def main():
     config = utils.read_config(os.path.join(dir_name, 'config.txt'), defaults)
     psql_connect_string = 'dbname=%s user=%s password=%s host=%s port=%s' % ( config['psql-database'], config['psql-user'], config['psql-password'], config['psql-host'], config['psql-port'] )
 
+    conn = psycopg2.connect(psql_connect_string)
+    c = conn.cursor()
 
-    build_base_data(psql_connect_string)
+    c.execute("select tablename from pg_tables where schemaname='public'" )
+    tables = c.fetchall()
+
+    needed_tables = ( 'graph_vertices', 'graph_payloads', 'graph_edges', 'graph_resources',
+                      'osm_nodes', 'osm_ways', 'osm_edges', 'gtfs_agency', 'gtfs_calendar',
+                      'gtfs_calendar_dates', 'gtfs_frequencies', 'gtfs_routes', 'gtfs_shapes',
+                      'gtfs_stop_times', 'gtfs_stops', 'gtfs_transfers', 'gtfs_trips' )
+
+    for nt in needed_tables:
+        if (nt,) not in tables:
+            overwrite = True
+
+    c.close()
+
+    if overwrite:
+        build_base_data(dir_name, psql_connect_string)
 
     g = GraphDatabase(psql_connect_string).incarnate()
 
     print('importing routing data...')
-    conn = psycopg2.connect(psql_connect_string)
-    conn.set_client_encoding('UTF8')
-
     import_route_data.read_times(times_filename, conn)
     import_route_data.read_points(points_filename, conn)
     import_route_data.read_routes(routes_filename, conn)
