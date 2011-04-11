b823d24100c3e552eedd0b5186e9c10738014a91
Tobias Ottenweller <tobi@localhost.(none)>
2011-04-11 21:28:39 +0200

    psycopg2 to setup.py added

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(  name='graphserver_tools',
         version='0.22',
         author='Tobias Ottenweller',
         packages=['graphserver_tools', 'graphserver_tools.utils', 'graphserver_tools.ext' ],
-        install_requires=['graphserver>=1.0.0','pyproj>=1.8.8',],
+        install_requires=['graphserver>=1.0.0','pyproj>=1.8.8', 'psycopg2' ],
 
         entry_points={
             'console_scripts': [
