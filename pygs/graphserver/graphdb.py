464333d28bb2150dbf85ad6183c5b430acb20237
Tobias Ottenweller <tobi@localhost.(none)>
2011-04-12 11:08:02 +0200

    graphdb port to psql

--- pygs/graphserver/graphdb.py
+++ pygs/graphserver/graphdb.py
@@ -1,5 +1,5 @@
 import os
-import sqlite3
+import psycopg2
 import cPickle
 from graphserver.core import State, Graph, Combination
 from graphserver import core
@@ -7,225 +7,267 @@ from sys import argv
 import sys
 
 class GraphDatabase:
-    
-    def __init__(self, sqlite_filename, overwrite=False):
-        if overwrite:
-            if os.path.exists(sqlite_filename):
-                os.remove( sqlite_filename )
-        elif not os.path.exists(sqlite_filename):
-            overwrite = True # force an init of the tables
-                
-        self.conn = sqlite3.connect(sqlite_filename)
-        
+
+    def __init__(self, psql_conn_string, overwrite=False):
+
+        self.conn = psycopg2.connect(psql_conn_string)
+
+        c = self.conn.cursor()
+        c.execute("select tablename from pg_tables where schemaname='public'" )
+        tables = c.fetchall()
+
+        for t in ( 'graph_vertices', 'graph_payloads', 'graph_edges', 'graph_resources' ):
+            if (t,) not in tables:
+                overwrite = True
+
         if overwrite:
             self.setup()
-            
+
+        # prepared inserts for better performance
+        c.execute( """PREPARE preparedGraphVertexInsert ( TEXT ) AS
+                      INSERT INTO graph_vertices ( label ) VALUES ( $1 )""" )
+
+        c.execute( """PREPARE preparedGraphEdgeInsert ( TEXT, TEXT, INTEGER ) AS
+                      INSERT INTO graph_edges ( vertex1, vertex2, epid ) VALUES ( $1, $2, $3 )""" )
+
+        c.execute( """PREPARE preparedPayloadInsert ( TEXT, TEXT ) AS
+                      INSERT INTO graph_payloads ( type, state ) VALUES ( $1, $2) RETURNING id""")
+
+        c.execute( """PREPARE preparedResourcesInsert ( TEXT, TEXT ) AS
+                      INSERT INTO graph_resources ( name, image ) VALUES ( $1, $2 )""")
+
+        c.close()
+
         self.resources_cache = {}
         self.payloads_cache = {}
-        
+
     def setup(self):
         c = self.conn.cursor()
-        c.execute( "CREATE TABLE vertices (label TEXT UNIQUE ON CONFLICT IGNORE)" )
-        c.execute( "CREATE TABLE payloads (id INTEGER PRIMARY KEY, type TEXT, state TEXT)" )
-        c.execute( "CREATE TABLE edges (vertex1 TEXT, vertex2 TEXT, epid INTEGER)" )
-        c.execute( "CREATE TABLE resources (name TEXT UNIQUE ON CONFLICT IGNORE, image TEXT)" )
-    
+        c.execute( "DROP TABLE IF EXISTS graph_vertices CASCADE" )
+        c.execute( "DROP TABLE IF EXISTS graph_payloads CASCADE" )
+        c.execute( "DROP TABLE IF EXISTS graph_edges CASCADE" )
+        c.execute( "DROP TABLE IF EXISTS graph_resources CASCADE" )
+
+        # fast version - duplicate database entries
+        c.execute( "CREATE TABLE graph_vertices (label TEXT )" )
+        c.execute( "CREATE TABLE graph_resources (name TEXT, image TEXT)" )
+        c.execute( "CREATE TABLE graph_payloads (id SERIAL, type TEXT, state TEXT)" )
+        c.execute( "CREATE TABLE graph_edges (vertex1 TEXT , vertex2 TEXT, epid INTEGER)" )
+
+
+        # much slower, but consistent version
+        '''c.execute( "CREATE TABLE graph_vertices (label TEXT PRIMARY KEY)" )
+        c.execute( "CREATE TABLE graph_resources (name TEXT PRIMARY KEY, image TEXT)" )
+        c.execute( "CREATE TABLE graph_payloads (id SERIAL PRIMARY KEY, type TEXT, state TEXT)" )
+        c.execute( "CREATE TABLE graph_edges (vertex1 TEXT REFERENCES graph_vertices, vertex2 TEXT REFERENCES graph_vertices, epid INTEGER REFERENCES graph_payloads)" )
+
+        c.execute( """CREATE OR REPLACE RULE ignore_duplicate_graph_vertices
+                      AS ON INSERT TO graph_vertices
+                      WHERE label IN ( SELECT label FROM graph_vertices)
+                      DO INSTEAD NOTHING""" )
+
+        c.execute( """CREATE OR REPLACE RULE ignore_duplicate_graph_resources
+                      AS ON INSERT TO graph_resources
+                      WHERE name IN ( SELECT name FROM graph_resources)
+                      DO INSTEAD NOTHING""" )'''
+
+
         self.conn.commit()
-        c.close()
-        
+
     def put_edge_payload(self, edgepayload, cc):
-        
+
         if edgepayload.__class__ == Combination:
-	    edge_state = []
+            edge_state = []
 
             for component in edgepayload.components:
                 rowid = self.put_edge_payload( component, cc )
 
-		edge_state.append( rowid )
+            edge_state.append( rowid )
         else:
-	    edge_state = edgepayload.__getstate__()
+            edge_state = edgepayload.__getstate__()
 
-        
-        cc.execute( "INSERT INTO payloads (type, state) VALUES (?, ?)", 
-	            ( cPickle.dumps( edgepayload.__class__ ), 
-		      cPickle.dumps( edge_state ) ) )
 
-	return cc.lastrowid
-        
+        cc.execute( "EXECUTE preparedPayloadInsert (%s, %s)", ( cPickle.dumps( edgepayload.__class__ ), cPickle.dumps( edge_state ) ) )
+
+        return cc.fetchone()
+
     def get_edge_payload(self, id):
-        queryresult = list(self.execute( "SELECT id, type, state FROM payloads WHERE id=?", (id,) ))
+        queryresult = list(self.execute( "SELECT id, type, state FROM graph_payloads WHERE id=%s", (id,) ))
         if len(queryresult)==0:
             return None
-        
+
         id, type, state = queryresult[0]
-        
+
         if id in self.payloads_cache:
             return self.payloads_cache[id]
-        
+
         typeclass = cPickle.loads( str(type) )
         ret = typeclass.reconstitute( cPickle.loads( str(state) ), self )
         ret.external_id = int(id)
         self.payloads_cache[id] = ret
         return ret
-        
+
     def populate(self, graph, reporter=None):
         c = self.conn.cursor()
-        
+
         n = len(graph.vertices)
-	nseg = max(n,100)
+        nseg = max(n,100)
         for i, vv in enumerate( graph.vertices ):
             if reporter and i%(nseg//100)==0: reporter.write( "%d/%d vertices dumped\n"%(i,n) )
-            
-            c.execute( "INSERT INTO vertices VALUES (?)", (vv.label,) )
+
+            c.execute( "EXECUTE preparedGraphVertexInsert (%s)", (vv.label,) )
             for ee in vv.outgoing:
                 epid = self.put_edge_payload( ee.payload, c )
 
-                c.execute( "INSERT INTO edges VALUES (?, ?, ?)", (ee.from_v.label, ee.to_v.label, epid) )
+                c.execute( "EXECUTE  preparedGraphEdgeInsert (%s, %s, %s)", (ee.from_v.label, ee.to_v.label, epid) )
 
                 if hasattr(ee.payload, "__resources__"):
                     for name, resource in ee.payload.__resources__():
 
                         self.store( name, resource, c )
-        
+
         self.conn.commit()
         c.close()
-        
+
         self.index()
-        
+
     def get_cursor(self):
         return self.conn.cursor()
+
     def commit(self):
         self.conn.commit()
-        
+
     def add_vertex(self, vertex_label, outside_c=None):
         c = outside_c or self.conn.cursor()
-        
-        c.execute( "INSERT INTO vertices VALUES (?)", (vertex_label,) )
-        
+
+        c.execute( "EXECUTE preparedGraphVertexInsert (%s)", (vertex_label,) )
+
         if outside_c is None:
             self.conn.commit()
             c.close()
-            
+
     def remove_edge( self, oid, outside_c=None ):
         c = outside_c or self.conn.cursor()
-        
-        c.execute( "DELETE FROM edges WHERE oid=?", (oid,) )
-        
+
+        c.execute( "DELETE FROM graph_edges WHERE oid=%s", (oid,) )
+
         if outside_c is None:
             self.conn.commit()
             c.close()
-        
+
     def add_edge(self, from_v_label, to_v_label, payload, outside_c=None):
         c = outside_c or self.conn.cursor()
-    
+
         epid = self.put_edge_payload( payload, c )
-        c.execute( "INSERT INTO edges VALUES (?, ?, ?)", (from_v_label, to_v_label, epid) )
-    
+        c.execute( "EXECUTE  preparedGraphEdgeInsert (%s, %s, %s)", (from_v_label, to_v_label, epid) )
+
         if hasattr(payload, "__resources__"):
             for name, resource in payload.__resources__():
                 self.store( name, resource )
-    
+
         if outside_c is None:
             self.conn.commit()
             c.close()
-        
+
     def execute(self, query, args=None):
-        
+
         c = self.conn.cursor()
-        
+
         if args:
             c.execute( query, args )
         else:
             c.execute( query )
-            
+
         for record in c:
             yield record
         c.close()
-        
+
     def all_vertex_labels(self):
-        for vertex_label, in self.execute( "SELECT DISTINCT label FROM (SELECT vertex1 AS label FROM edges UNION SELECT vertex2 AS label FROM edges)" ):
+        for vertex_label, in self.execute( "SELECT DISTINCT label FROM (SELECT vertex1 AS label FROM graph_edges UNION SELECT vertex2 AS label FROM graph_edges) AS label" ):
             yield vertex_label
-    
+
     def all_edges(self):
-        for vertex1, vertex2, epid in self.execute( "SELECT vertex1, vertex2, epid FROM edges" ):
+        for vertex1, vertex2, epid in self.execute( "SELECT vertex1, vertex2, epid FROM graph_edges" ):
             ep = self.get_edge_payload( epid )
-            
+
             yield vertex1, vertex2, ep
-    
+
     def all_outgoing(self, vertex1_label):
-        for vertex1, vertex2, epid in self.execute( "SELECT vertex1, vertex2, epid FROM edges WHERE vertex1=?", (vertex1_label,) ):
+        for vertex1, vertex2, epid in self.execute( "SELECT vertex1, vertex2, epid FROM graph_edges WHERE vertex1=%s", (vertex1_label,) ):
             yield vertex1, vertex2, self.get_edge_payload( epid )
-    
+
     def all_incoming(self, vertex2_label):
-        for vertex1, vertex2, epid in self.execute( "SELECT vertex1, vertex2, epid FROM edges WHERE vertex2=?", (vertex2_label,) ):
+        for vertex1, vertex2, epid in self.execute( "SELECT vertex1, vertex2, epid FROM graph_edges WHERE vertex2=%s", (vertex2_label,) ):
             yield vertex1, vertex2, self.get_edge_payload( epid )
-    
-            
+
+
     def store(self, name, obj, c=None):
         cc = self.conn.cursor() if c is None else c
-        resource_count = list(cc.execute( "SELECT count(*) FROM resources WHERE name = ?", (name,) ))[0][0]
-        if resource_count == 0:
-            cc.execute( "INSERT INTO resources VALUES (?, ?)", (name, cPickle.dumps( obj )) )
-            if not c: self.conn.commit()
-        if not c: cc.close()
-        
+
+        cc.execute( "INSERT INTO graph_resources VALUES (%s, %s)", (name, cPickle.dumps( obj )) )
+
+        if not c:
+            self.conn.commit()
+            cc.close()
+
     def resolve(self, name):
         if name in self.resources_cache:
             return self.resources_cache[name]
         else:
-            image = list(self.execute( "SELECT image FROM resources WHERE name = ?", (str(name),) ))[0][0]
+
+            image = list(self.execute( "SELECT image FROM graph_resources WHERE name = %s", (str(name),) ))[0][0]
             resource = cPickle.loads( str(image) )
             self.resources_cache[name] = resource
             return resource
-        
+
     def resources(self):
-        for name, image in self.execute( "SELECT name, image from resources" ):
+        for name, image in self.execute( "SELECT name, image from graph_resources" ):
             yield name, cPickle.loads( str(image) )
-            
+
     def index(self):
         c = self.conn.cursor()
-        c.execute( "CREATE INDEX vertices_label ON vertices (label)" )
-        c.execute( "CREATE INDEX ep_ids ON payloads (id)" )
+        c.execute( "CREATE INDEX graph_vertices_label ON graph_vertices (label)" )
+        c.execute( "CREATE INDEX ep_ids ON graph_payloads (id)" )
         self.conn.commit()
         c.close()
-        
+
     def num_vertices(self):
-        return list(self.execute( "SELECT count(*) from vertices" ))[0][0]
-        
+        return list(self.execute( "SELECT count(*) from graph_vertices" ))[0][0]
+
     def num_edges(self):
-        return list(self.execute( "SELECT count(*) from edges" ))[0][0]
-        
+        return list(self.execute( "SELECT count(*) from graph_edges" ))[0][0]
+
     def incarnate(self, reporter=sys.stdout):
         g = Graph()
         num_vertices = self.num_vertices()
-        
+
         for i, vertex_label in enumerate( self.all_vertex_labels() ):
-            if reporter and i%5000==0: 
-                reporter.write("\r%d/%d vertices"%(i,num_vertices) ) 
+            if reporter and i%5000==0:
+                reporter.write("\r%d/%d vertices"%(i,num_vertices) )
                 reporter.flush()
             g.add_vertex( vertex_label )
-        
+
         if reporter: reporter.write("\rLoaded %d vertices %s\n" % (num_vertices, " "*10))
-        
+
         num_edges = self.num_edges()
         for i, (vertex1, vertex2, edgetype) in enumerate( self.all_edges() ):
-            if i%5000==0: 
-                reporter.write("\r%d/%d edges"%(i,num_edges) ) 
+            if i%5000==0:
+                reporter.write("\r%d/%d edges"%(i,num_edges) )
                 reporter.flush()
             g.add_edge( vertex1, vertex2, edgetype )
         if reporter: reporter.write("\rLoaded %d edges %s\n" % (num_edges, " "*10))
-        
+
         return g
-        
+
 
 def main():
     if len(argv) < 2:
         print "usage: python graphdb.py [vertex1, [vertex2]]"
         return
-    
+
     graphdb_filename = argv[1]
     graphdb = GraphDatabase( graphdb_filename )
-    
+
     if len(argv) == 2:
         print "vertices:"
         for vertex_label in sorted( graphdb.all_vertex_labels() ):
@@ -234,11 +276,11 @@ def main():
         for name, resource in graphdb.resources():
             print name, resource
         return
-    
+
     vertex1 = argv[2]
     for vertex1, vertex2, edgetype in graphdb.all_outgoing( vertex1 ):
         print "%s -> %s\n\t%s"%(vertex1, vertex2, repr(edgetype))
-        
+
         if len(argv) == 4:
             s0 = State(1,int(argv[3]))
             print "\t"+str(edgetype.walk( s0 ))
