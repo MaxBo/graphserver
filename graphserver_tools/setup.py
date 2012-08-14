a2177b30d35605e6d91f6fff8e89a4a5edd0d642
achim <tack@ggr-planung.de>
2012-08-14 13:58:47 +0200

    several updates

--- graphserver_tools/setup.py
+++ graphserver_tools/setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 setup(  name='graphserver_tools',
         version='0.52',
         author='Tobias Ottenweller, Max Bohnet, Christine Walther, Achim Tack',
-        packages=['graphserver_tools', 'graphserver_tools.utils', 'graphserver_tools.ext', 'graphserver_tools.ext.ivu', 'graphserver_tools.ext.ivu.models' ],
+        packages=['graphserver_tools', 'graphserver_tools.utils', 'graphserver_tools.ext',
+         'graphserver_tools.ext.dino', 'graphserver_tools.ext.dino.models',
+         'graphserver_tools.ext.ivu', 'graphserver_tools.ext.ivu.models' ],
         install_requires=['graphserver>=1.0.0','pyproj>=1.8.8', 'psycopg2', 'termcolor', 'transitfeed', 'sqlalchemy', 'bitstring' ],
 
         entry_points={
@@ -14,6 +16,7 @@ setup(  name='graphserver_tools',
                 'gst_cropOSM = graphserver_tools.ext.cropOSM:main',
                 'gst_gtfsToVisum = graphserver_tools.ext.gtfToVisum:main',
                 'gst_ivuToVisum = graphserver_tools.ext.ivuToVisum:main',
+                'gst_dinoToVisum = graphserver_tools.ext.dinoToVisum:main',
             ],
         }
      )
